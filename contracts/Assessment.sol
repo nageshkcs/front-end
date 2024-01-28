3728da33c323e8660fc19249a2eca95e2b6035d8
nageshkcs <156075543+nageshkcs@users.noreply.github.com>
2024-01-29 00:01:48 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -9,33 +9,27 @@ contract Assessment {
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event ReceiptGenerated(address indexed recipient, uint256 amount);
+    event RewardDistributed(address indexed recipient, uint256 amount);
+    event RewardReplenished(uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -47,14 +41,25 @@ contract Assessment {
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function generateReceipt(address _recipient, uint256 _amount) public {
+        require(msg.sender == owner, "You are not authorized to generate receipts");
+        emit ReceiptGenerated(_recipient, _amount);
+    }
+
+    function distributeReward(address _recipient, uint256 _amount) public {
+        require(msg.sender == owner, "You are not authorized to distribute rewards");
+        balance -= _amount;
+        emit RewardDistributed(_recipient, _amount);
+    }
+
+    function replenishReward(uint256 _amount) public payable {
+        require(msg.sender == owner, "You are not authorized to replenish rewards");
+        balance += _amount;
+        emit RewardReplenished(_amount);
     }
 }
